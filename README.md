7401da66bffb23b53b07875e1dfb6697765280ff
JeremyCS12 <jeremy.ortiz26@compscihigh.org>
2026-01-21 17:39:14 +0000

    Finished API

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 Why did you pick this API topic?
 
-
+I picked this API topic because I go to the gym everyday so all the information came to me natural and I didn't really need to think much when creating the planning doc.
 
 Describe your API and the available data endpoints
 
-
+My API is supposed to give a variety of workouts so the user can pick from and do themselves.
+"/" introduces the user to the website, "/docs" tells the user the endpoints they need for the data. "/api/workouts" Gives the user all the workouts they can do incase they don't want to find a specific one. "/api/workouts/:id" Will give the user a specific workout based on the id they pick.
 
 
 How was your experience building your own API server after using other APIs as a client in Unit 2?
+
+It feels annoying and I hope I can continue to use APIs created by others instead of creating them myself. I find it nice but it is tedious. 
--- index.js
+++ index.js
@@ -1,1 +1,117 @@
+const express = require("express")
+const app = express()
 
+app.use((req,res,next) => {
+console.log(req.method, req.url)
+next()
+})
+
+
+const workouts = [
+  {
+    id: 1,
+    name: "Push-Ups",
+    muscleGroup: "Chest",
+    difficulty: "Beginner",
+    duration: 10,
+    equipment: false
+  },
+  {
+    id: 2,
+    name: "Squats",
+    muscleGroup: "Legs",
+    difficulty: "Beginner",
+    duration: 15,
+    equipment: false
+  },
+  {
+    id: 3,
+    name: "Plank Hold",
+    muscleGroup: "Core",
+    difficulty: "Beginner",
+    duration: 5,
+    equipment: false
+  },
+  {
+    id: 4,
+    name: "Bench Press",
+    muscleGroup: "Chest",
+    difficulty: "Intermediate",
+    duration: 20,
+    equipment: true
+  },
+  {
+    id: 5,
+    name: "Deadlift",
+    muscleGroup: "Back",
+    difficulty: "Advanced",
+    duration: 25,
+    equipment: true
+  },
+  {
+    id: 6,
+    name: "Pull-Ups",
+    muscleGroup: "Back",
+    difficulty: "Intermediate",
+    duration: 15,
+    equipment: true
+  },
+  {
+    id: 7,
+    name: "Lunges",
+    muscleGroup: "Legs",
+    difficulty: "Beginner",
+    duration: 12,
+    equipment: false
+  },
+  {
+    id: 8,
+    name: "Shoulder Press",
+    muscleGroup: "Shoulders",
+    difficulty: "Intermediate",
+    duration: 18,
+    equipment: true
+  },
+  {
+    id: 9,
+    name: "Bicep Curls",
+    muscleGroup: "Arms",
+    difficulty: "Beginner",
+    duration: 10,
+    equipment: true
+  },
+  {
+    id: 10,
+    name: "Mountain Climbers",
+    muscleGroup: "Full Body",
+    difficulty: "Intermediate",
+    duration: 8,
+    equipment: false
+  }
+];
+
+
+app.get("/", (req,res ) => {
+    res.status(200).send("<h1>Welcome! This is a fitness workout API where you can see different workouts.</h1>")
+})
+
+app.get("/docs", (req,res ) => {
+    res.status(200).send("<h1>You can use {/api/workouts} to see every workout. For individual ones you can use {/api/workouts/:id}   </h1>")
+})
+
+app.get("/api/workouts", (req,res ) => {
+    res.status(200).json(workouts)
+})
+
+app.get("/api/workouts/:id", (req,res ) => {
+    res.status(200).json(workouts[req.params.id - 1])
+})
+
+app.use((req, res) => {
+    res.status(404).send("<h1>404 Not Found</h1>")
+    console.log("404 Error")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running")
+})
